9a7f040494fd1370217d221a1019f6c7899294e8
grace-ataro <gracemaloba034@gmail.com>
2023-04-27 15:35:19 +0300

    myduka

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from flask import Flask,render_template
+
+import psycopg2
+
 app=Flask(__name__)
+
+try:
+    conn=psycopg2.connect("dbname='myduka' user='postgres' host='localhost' password='18090000'")
+    print("Database connected succesfully")
+except Exception as e:
+    print("I am unable to connect to database",e)    
 @app.route("/")
 def home():
     username="Grace Ataro"
     return render_template("index.html",username=username)
-app.run()
+
+
 @app.route("/products")
 def products():
-    return render_template("products.html")
+    
+    # products=[
+    #     (1,"omo",40,50,100,),
+    #     (2,"bread",50,60,200,),
+    #     (3,"milk",60,65,150,)
+    # ]
+ cur = conn.cursor()
+ cur.execute("Insert into products(id,name.buyingprice,sellingprice,quantity)")
+ rows = cur.fetchall()
+ print("rows")
+ return ("products.html",rows=rows)
+
 app.run()
